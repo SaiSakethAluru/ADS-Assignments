08ef01c52caec6a83c8ee0b0d9e94e69f924c757
Saketh <alurusaisaketh@gmail.com>
2020-09-13 08:36:33 +0530

    Loading tree before search

--- Assignment 1/search.cpp
+++ Assignment 1/search.cpp
@@ -1,5 +1,45 @@
 #include "RTree.h"
 
+RTreeNode* read_node(fstream& f, int m, int M, int n)
+{
+    int node_id,num_entries,isLeaf;
+    f>>node_id>>num_entries>>isLeaf;
+    RTreeNode* node = new RTreeNode(m,M,n);
+    node->num_entries = num_entries;
+    node->node_id = node_id;
+    node->isLeaf = isLeaf;
+    for(int i=0;i<n;i++){
+        f>>node->bounds[i].first>>node->bounds[i].second;
+    }
+    return node;
+
+}
+RTree* load_tree(string filename)
+{
+    fstream f(filename);
+    int num_nodes;
+    f>>num_nodes;
+    vector<int> parent(num_nodes);
+    int m, M, n;
+    f>>m>>M>>n;
+    RTree* tree = new RTree(n);
+    tree->num_nodes = num_nodes;
+    for(int i=0;i<num_nodes;i++){
+        f>>parent[i];
+    }
+    vector<RTreeNode*> nodes;
+    for(int i=0;i<num_nodes;i++){
+        nodes.push_back(read_node(f,m,M,n));
+    }
+    for(int i=0;i<num_nodes;i++){
+        if(parent[i]!=-1){
+            nodes[parent[i]]->pointers[nodes[parent[i]]->num_entries] = nodes[i];
+            nodes[parent[i]]->num_entries++;
+        }
+        else tree->root = nodes[i];
+    }
+    return tree;
+}
 bool check_overlap(vector<pair<int,int> > &bounds, vector<pair<int,int> > &region)
 {
     int i;
@@ -52,7 +92,7 @@ RTreeNode* search(RTreeNode *root,vector<pair<int,int>> &region,int M)
 int main()
 {
     // Read File and make the tree
-    RTree* Tree; // The tree made after reading from input
+    RTree* Tree = load_tree("RTree.txt"); // The tree made after reading from input
     int M = Tree->M;
     vector<pair<int,int>> region; // The region to be searched
     RTreeNode* node = search(Tree->root,region,M);
