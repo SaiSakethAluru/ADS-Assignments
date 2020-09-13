30f42ab945328028fda4fbc1c109165e31649f0a
Saketh <alurusaisaketh@gmail.com>
2020-09-13 17:05:48 +0530

    fix class constructor references in search.cpp

--- Assignment 1/search.cpp
+++ Assignment 1/search.cpp
@@ -1,5 +1,35 @@
 #include "RTree.h"
 
+RTreeNode::RTreeNode(int m, int M, int n)
+{
+    static int id = 0;
+    this->m = m;
+    this->M = M;
+    this->n = n;
+    this->pointers = vector<RTreeNode*>(M,nullptr);
+    this->isLeaf = true;
+    this->num_entries = 0;
+    this->node_id = id++;
+    this->bounds = vector<pair<int,int> > (n);
+}
+
+RTreeNode::~RTreeNode()
+{
+}
+
+RTree::RTree(int n)
+{
+    this->n = n;
+    this->root = nullptr;
+    this->M = 4096/(4*n+1);
+    this->m = this->M/2;
+    this->num_nodes = 0;
+}
+
+RTree::~RTree()
+{
+}
+
 RTreeNode* read_node(fstream& f, int m, int M, int n)
 {
     int node_id,num_entries,isLeaf;
