0302d7b780c7dc4141f9e68a38687fa3cfd384a2
Saketh <alurusaisaketh@gmail.com>
2020-09-13 16:43:27 +0530

    main file for insertion

---
+++ Assignment 1/main.cpp
@@ -0,0 +1,42 @@
+#include "RTree.h"
+#include <iostream>
+
+using namespace std;
+
+RTreeNode::RTreeNode(int m, int M, int n)
+{
+    static int id = 0;
+    this->m = m;
+    this->M = M;
+    this->n = n;
+    this->pointers = vector<RTreeNode*>(M,nullptr);
+    this->isLeaf = true;
+    this->num_entries = 0;
+    this->node_id = id++;
+    this->bounds = vector<pair<int,int> > (n);
+}
+
+RTreeNode::~RTreeNode()
+{
+}
+
+RTree::RTree(int n)
+{
+    this->n = n;
+    this->root = nullptr;
+    this->M = 4096/(4*n+1);
+    this->m = this->M/2;
+    this->num_nodes = 0;
+}
+
+RTree::~RTree()
+{
+}
+
+int main()
+{
+    int n = 2;
+    RTree* tree = new RTree(n);
+    tree->insert("data.txt","n2Tree.txt");
+    return 0;
+}
